632fc91b9b7d0c02ba6277534b53b243a793a215
Ibrahim Mammadov <zxyctn@gmail.com>
2022-12-27 23:40:13 +0100

    Final touches for the website

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -5,7 +5,7 @@
 <div class="navbar bg-primary text-primary-content">
   <div class="container mx-auto">
     <div class="flex-1">
-      <a href="/" class="btn btn-ghost normal-case text-xl">Trellone</a>
+      <a href="/trellone" class="btn btn-ghost normal-case text-xl">Trellone</a>
     </div>
     <div class="flex-none">
       <DarkModeToggle />
